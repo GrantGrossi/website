cb8a11879e5ef30c973b61bbc7cb14524a4314a7
Green <Green@DESKTOP-8NS8T8U>
2024-08-21 18:24:55 -0700

    Experimenting with Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './Custom.css';
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import Biography from './biography';
+import Home from './Home';
 
 function App() {
+  /*
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          Test
         </p>
         <a
           className="App-link"
@@ -22,6 +25,47 @@ function App() {
           Learn React
         </a>
       </header>
+    </div>
+  );
+  */
+  return (
+    <div>
+      <Header />
+
+    </div>
+  )
+}
+
+function Temp() {
+  return (
+    <div>Temp</div>
+  );
+}
+
+function Header() {
+  return (
+    <div className="Header">
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/biography">Biography</Link>
+            </li>
+          </ul>
+
+          <hr />
+          <Routes>
+            <Route path="/" Component={Home} />
+            <Route path="/biography" Component={Biography} />
+          </Routes>
+
+
+        </div>
+      </Router>
+      
     </div>
   );
 }
---
+++ src/biography.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+function Biography() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Biography page
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+
+export default Biography;
