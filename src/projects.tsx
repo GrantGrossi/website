a5055d4de8fa1034aaaf1a35100a5a4800e698b4
Green <Green@DESKTOP-8NS8T8U>
2024-09-05 14:36:07 -0700

    Began work on projects page

--- src/projects.tsx
+++ src/projects.tsx
@@ -1,10 +1,38 @@
 import React from 'react';
+import debugImage from './tex_DebugUVTiles.png';
+import './projects.css';
 
 
 function Projects() {
   return (
     <div>
       Projects Page
+      <div className='Projects'>
+        <div className="Project-Container">
+            <img src={debugImage} alt="Debug Texture" />
+        </div>
+        <div className="Project-Container">
+            <img src={debugImage} alt="Debug Texture" />
+        </div>
+        <div className="Project-Container">
+            <img src={debugImage} alt="Debug Texture" />
+        </div>
+        <div className="Project-Container">
+            <img src={debugImage} alt="Debug Texture" />
+        </div>
+        <div className="Project-Container">
+            <img src={debugImage} alt="Debug Texture" />
+        </div>
+        <div className="Project-Container">
+            <img src={debugImage} alt="Debug Texture" />
+        </div>
+        <div className="Project-Container">
+            <img src={debugImage} alt="Debug Texture" />
+        </div>
+        <div className="Project-Container">
+            <img src={debugImage} alt="Debug Texture" />
+        </div>
+      </div>
     </div>
   );
 }
